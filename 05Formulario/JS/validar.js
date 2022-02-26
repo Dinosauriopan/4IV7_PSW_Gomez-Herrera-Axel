e15dcd379f99587ecae0652c2781bed248fbca06
Dinosauriopan <agomezh2003@alumno.ipn.mx>
2022-02-25 18:38:38 -0600

    validacion js

--- 05Formulario/JS/validar.js
+++ 05Formulario/JS/validar.js
@@ -9,4 +9,71 @@ function validar(formulario){
         formulario.nombre.focus();
     return false;
     }
+
+    /*
+    Validar que acepte unicamente letras
+    */
+
+    var checkOk = "QWERTYUIOPASDFGHJKLÑZXCVBNM" + "qwertyuiopasdfghjklñzxcvbnm";
+
+    var checkStr = formulario.nombre.value;
+
+    var allValid = true;
+
+    for(var i = 0; i < checkStr.length; i++){
+        var ch = checkStr.charAt(i);
+        for(var j = 0; j < checkOk.length; j++)
+        if(ch == checkOk.charAt(j));
+            break;
+        
+        if(j == checkOk.length){
+            allValid = false;
+            break;
+        }
+    }
+
+    if(!allValid){
+        alert("Escribe solo letras en el campo Nombre");
+        formulario.nombre.focus();
+        return false;
+    }
+
+    //Validar solo numeros
+
+    var checkOk = "1234567890";
+
+    var checkStr = formulario.edad.value;
+
+    var allValid = true;
+
+    for(var i = 0; i < checkStr.length; i++){
+        var ch = checkStr.charAt(i);
+        for(var j = 0; j < checkOk.length; j++)
+        if(ch == checkOk.charAt(j));
+            break;
+        
+        if(j == checkOk.length){
+            allValid = false;
+            break;
+        }
+    }
+
+    if(!allValid){
+        alert("Escribe solo numeros en Edad");
+        formulario.edad.focus();
+        return false;
+    }
+
+    //validar correo electronico
+    //Expresiones regulares, poder diferenciar una cadena de caracteres
+    // y definir que caracteres son validos de acuerdo a una condicion
+
+    var txt = formulario.email.value;
+
+    //Expresion regular
+
+    var b = /^[^@\s]]+@[^@\.\s]+(\.[^@\.\s]+)+$/;
+
+    alert("Email " + (b.test(txt)?"":"no ") + "valido");
+    return b.test(txt);
 }
