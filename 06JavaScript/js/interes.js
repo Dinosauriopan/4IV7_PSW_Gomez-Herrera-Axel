c418edd8cc63cdf5ffcf7838295257d3327a70ef
Dinosauriopan <agomezh2003@alumno.ipn.mx>
2022-03-11 17:26:22 -0600

    interes js

---
+++ 06JavaScript/js/interes.js
@@ -0,0 +1,11 @@
+/* vamos a realizar una funcion que se encarge
+de validar la entrada de numeros por medio
+de una expresion regular.
+*/
+function validarn(e){
+    var teclado = (document.all)? e.keyCode : e.which;
+    if(teclado == 8) return true;
+    var patron = /[0-9\d .]/;
+    var codigo = String.fromCharCode(teclado);
+    return patron.test(codigo);
+}
