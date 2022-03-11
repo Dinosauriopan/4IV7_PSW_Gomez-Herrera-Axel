3b83783f5763ad9361c5d8f8c24375386f106070
Dinosauriopan <agomezh2003@alumno.ipn.mx>
2022-03-11 17:38:05 -0600

    js

--- 06JavaScript/js/interes.js
+++ 06JavaScript/js/interes.js
@@ -9,3 +9,12 @@ function validarn(e){
     var codigo = String.fromCharCode(teclado);
     return patron.test(codigo);
 }
+
+function interes(){
+    var valor =  document.formulario.cantidad.value;
+    var resul = parseInt(valor);
+    var interes = resul*0.037;
+    var total = interes + resul;
+    
+    document.formulario.sueldoI.value = "$" + total;
+}
